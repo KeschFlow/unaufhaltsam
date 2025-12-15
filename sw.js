719081e14e8f884b0b4a3a879377262a89ad51a9
KeschFlow <keschflow@gmail.com>
2025-12-15 10:22:29 +0100

    Add service worker for Eddie Unaufhaltsam PWA
    This service worker caches specified assets during installation and serves them from the cache when requested.

--- sw.js
+++ sw.js
@@ -1,1 +1,34 @@
+const CACHE_NAME = "eddie-unaufhaltsam-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
